1a85486e8b753b740c72c711bcf2cdce6cec6cd1
Yohann THEODORE <yohann.theodore@gmail.com>
2026-02-20 14:41:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9198103303-d46bc",
+  "appId": "1:265016221216:web:3134f6b1a5eaf111a0e645",
+  "apiKey": "AIzaSyBMWOk2-EKrv7X7vL4tpFGsgHLYMf0j0xk",
+  "authDomain": "studio-9198103303-d46bc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "265016221216"
+};
